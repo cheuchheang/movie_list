8653d111ee52ae0396c53238e8e179895eaa9ab8
searcheuchheang <searcheuchheang@gmail.com>
2022-04-28 15:08:51 +0700

    create header and footer

---
+++ movie_list/src/components/Footer/Footer.tsx
@@ -0,0 +1,13 @@
+import React from "react";
+import "./Footer.scss";
+
+const Footer = () => {
+  return (
+    <div className="footer">
+      <div>Movie app</div>
+      <div>Address: #75 Teuk Tla, Khan Sen Sok, Phnom Penh</div>
+    </div>
+  );
+};
+
+export default Footer;
