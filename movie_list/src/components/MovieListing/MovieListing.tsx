60a8771f43f3491f53c8b921008fa8eac5390d8f
searcheuchheang <searcheuchheang@gmail.com>
2022-04-29 00:36:08 +0700

    fetch movie from omdbapi

--- movie_list/src/components/MovieListing/MovieListing.tsx
+++ movie_list/src/components/MovieListing/MovieListing.tsx
@@ -1,9 +1,22 @@
-import React from 'react'
+import React, { useEffect } from "react";
+import MovieApi from "common/api/MovieApi";
+import { APIKey } from "common/api/MovieApiKey";
 
 const MovieListing = () => {
-  return (
-	<div>MovieListing</div>
-  )
-}
+  useEffect(() => {
+    const movieText = "Harry";
+    const fetchMovies = async () => {
+      const response = await MovieApi.get(
+        `?apiKey=${APIKey}&s=${movieText}&type=movie`
+      ).catch((err)=>{
+        console.log("Err:",err);
+        
+      })
+      console.log("Respone:",response); 
+    };
+    fetchMovies()
+  },[]);
+  return <div className="banner-img"></div>;
+};
 
-export default MovieListing
+export default MovieListing;
